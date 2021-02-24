799d7afc8049f31efcafa3a6a3efc1e269dbf3b0
martijn <79571727+martijnsark@users.noreply.github.com>
2021-02-24 11:43:16 +0100

    Add files via upload

---
+++ 5maart-project.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace project_5maart_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read out the rules to the players
+            //read that max players = 4
+            //wrong user input cause error messages
+            // maby ask players to give themself a name inside the game?
+            //set number of players with array from 0 to 3 = 4players
+            //if players > 4 loop back to menu
+            //ask player to input anykey to continue after they read the rules
+            //players throw each 2 random dicecubes from 0 to 7 == 6 sides if extra time make more dicecubs and let the player select 1 of choice
+            //show the score board each round till round 4 
+            //if round 4 is over count all points for each player 
+            //show from top to bottem and high to low for all players score
+            //show player with highscore/mvp 
+            //end the game ask the player for anykey input to exit
+            //loop back to the menu
+        }
+    }
+}
